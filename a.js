d313ae7e3e5baa6db783591a64e12ea3276fd713
ankitjeevijay <ankitjeevijay@gmail.com>
2023-02-22 18:19:24 +0530

    learn by pninfosis

---
+++ a.js
@@ -0,0 +1,26 @@
+let car="bmw";
+
+console.log(car);
+ 
+ let van=["bmw",12,true,null,undefined,"bmw",12,true,null,undefined,"bmw",12,true,null,undefined];
+
+
+//console.log(van);
+//console.log(van[2]);
+//console.log(van.length);
+ const a=10
+ const b=4;
+ const c=a+b;
+ //console.log(c);
+ if (a==b) {
+    console.log("yes")
+ }
+ else{
+    console.log(no)
+ }
+// if (a!=b) {
+ //   console.log("yes")
+// }
+// else{
+ //   console.log("no")
+ //}
